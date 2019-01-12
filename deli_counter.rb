5ce434b9699412ece00db9ded06f100ccda99b79
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-12 02:14:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,7 @@ def take_a_number (array, name)
   array.push(name)
   position=array.index(name)
   puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
-   return name, position
  end
-
 
 def line(array)
   if array.length == 0
