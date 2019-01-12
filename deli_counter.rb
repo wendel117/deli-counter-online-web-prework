520fdab7f57dfd416bc0febc807729cce3d7a084
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-12 02:13:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,14 @@
 # Write your code here.
+katz_deli = []
+
 def take_a_number (array, name)
   array.push(name)
   position=array.index(name)
   puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
    return name, position
  end
-end
+
+
 def line(array)
   if array.length == 0
     puts "The line is currently empty."
