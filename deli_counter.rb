41a959aa7e655f331bf7968fcd4ac6475efd0987
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-12 02:11:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,3 +4,16 @@ def take_a_number (array, name)
   position=array.index(name)
   puts "Welcome, #{name}. You are number #{position} in line."
 end
+
+def line(array)
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    message = "The line is currently:"
+    array.each_with_index do |value, index| 
+   message += " #{index.to_i+1}. #{value}"
+ end 
+ puts "#{message}"
+ end 
+end 
+  
