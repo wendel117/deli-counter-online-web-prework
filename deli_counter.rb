8571a4c2e5ae318efe462bf5eec556f0275a03a7
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-12 02:17:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,4 +20,12 @@ def line(array)
  end
 end
 
-#def take_a_number()
+def now_serving(array)
+  if array.empty? == true 
+    puts "There is nobody waiting to be served!"
+  elsif array.empty? == false 
+    puts "Currently serving #{array.shift}."
+  end 
+end 
+
+now_serving(katz_deli)
